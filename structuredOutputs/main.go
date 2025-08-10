67ce00b4624630c30f99e1f093b91936a0f69971
Loh Siu Yin <siuyin@beyondbroadcast.com>
2025-08-10 20:30:52 +0800

    feat: add structured output

---
+++ structuredOutputs/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/firebase/genkit/go/ai"
+	"github.com/firebase/genkit/go/genkit"
+	"github.com/firebase/genkit/go/plugins/compat_oai/openai"
+	"github.com/openai/openai-go/option"
+	"github.com/siuyin/dflt"
+)
+
+type MenuItem struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Calories    int      `json:"calories"`
+	Allergens   []string `json:"allergens"`
+}
+
+func main() {
+	baseURL := dflt.EnvString("BASE_URL", "http://localhost:11434/v1")
+	modelName := dflt.EnvString("MODEL", "mistral")
+	log.Printf("BASE_URL=%s MODEL=%s", baseURL, modelName)
+
+	ctx := context.Background()
+	var err error
+
+	mySvr := &openai.OpenAI{APIKey: "Ollama", Opts: []option.RequestOption{option.WithBaseURL(baseURL)}}
+
+	g, err := genkit.Init(ctx, genkit.WithPlugins(mySvr))
+	if err != nil {
+		log.Fatalf("could not initialize Genkit: %v", err)
+	}
+	model, err := mySvr.DefineModel(g, modelName,
+		ai.ModelInfo{Supports: &ai.ModelSupports{Multiturn: true, SystemRole: true, Tools: false, Media: false}},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	timeout, cancel := context.WithTimeout(ctx, 120*time.Second)
+	defer cancel()
+
+	resp, err := genkit.Generate(timeout, g, ai.WithModel(model),
+		ai.WithPrompt("Invent a menu item for a pirate themed restaurant."),
+		ai.WithOutputType(&MenuItem{}),
+		// ai.WithStreaming(func(ctx context.Context, chunk *ai.ModelResponseChunk) error {
+		// 	fmt.Print(chunk.Text())
+		// 	return nil
+		// }),
+	)
+	if err != nil {
+		log.Println("generate: ", err) // ollama models like gemma3:1b output ```json. Even mistral fails to parse into MenuItem
+	}
+
+	var item MenuItem
+	if err := resp.Output(&item); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("%s (%d calories, %d allergens): %s\n",
+		item.Name, item.Calories, len(item.Allergens), item.Description)
+}
